a6800e6f46f7de1c21fb4ff433bdbb03c9b60da0
Ivan Anishchenko <ivananisenko08@gmail.com>
2023-11-11 13:59:23 +0300

    Add tests

--- src/main/java/org/example/controller/response/CommentCreateResponse.java
+++
@@ -1,6 +0,0 @@
-package org.example.controller.response;
-
-import org.example.entity.id.CommentId;
-
-public record CommentCreateResponse(CommentId commentId) {
-}
---
+++ src/test/java/org/example/testresponse/ArticleBuilder.java
@@ -0,0 +1,4 @@
+package org.example.testresponse;
+
+public class ArticleBuilder {
+}
